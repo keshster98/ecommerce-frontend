f70f7da0f6b973eb43d2c0ea32d5d929d5df64c4
Keshen <njkeshen98@gmail.com>
2025-01-16 14:07:50 +0800

    Made project ready for either dev or prod in constants.js file

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
 // Assign the main ecommerce_backend URL to API_URL
-export const API_URL = "http://localhost:5555";
+// NODE_ENV stores either "development" or "production"
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5555/api"
+    : "b13-keshen24.mak3r.dev/api";
