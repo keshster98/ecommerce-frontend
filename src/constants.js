f10ab44c9c40d68b3320c2caf532f9eae27ed734
Keshen <njkeshen98@gmail.com>
2025-01-19 14:44:26 +0800

    Revert "Made project ready for either dev or prod in constants.js file"
    This reverts commit f70f7da0f6b973eb43d2c0ea32d5d929d5df64c4.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,2 @@
 // Assign the main ecommerce_backend URL to API_URL
-// NODE_ENV stores either "development" or "production"
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5555/api"
-    : "b13-keshen24.mak3r.dev/api";
+export const API_URL = "http://localhost:5555";
